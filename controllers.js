2a234fc6d1261c16315a1ac01194493cef241edc
saddamhr <saddambubt65@gmail.com>
2020-10-01 13:45:51 +0600

    update and delete api created

--- controllers.js
+++ controllers.js
@@ -1,11 +1,30 @@
 const Contact = require('./Contact')
 
 exports.getAllContact = (req, res) => {
-
+    Contact.find()
+        .then(contacts => {
+            res.json(contacts)
+        })
+        .catch(e => {
+            console.log(e)
+            res.json({
+                message: 'Error Occurred'
+            })
+        })
 }
 
 exports.getSingleContact = (req, res) => {
-
+    let { id } = req.params
+    Contact.findById(id)
+        .then(contact => {
+            res.json(contact)
+        })
+        .catch(e => {
+            console.log(e)
+            res.json({
+                message: 'Error Occurred'
+            })
+        })
 }
 
 exports.createContact = (req, res) => {
@@ -28,9 +47,39 @@ exports.createContact = (req, res) => {
 }
 
 exports.updateContact = (req, res) => {
+    let { name, email, phone } = req.body
+    let { id } = req.params
 
+    Contact.findOneAndUpdate(
+        { _id: id },
+        {
+            $set: {
+                name, email, phone
+            }
+        },
+        { new: true }
+    )
+        .then(contact => {
+            res.json(contact)
+        })
+        .catch(e => {
+            console.log(e)
+            res.json({
+                message: 'Error Occurred'
+            })
+        })
 }
 
 exports.deleteContact = (req, res) => {
-
+    let { id } = req.params
+    Contact.findOneAndDelete({ _id: id })
+        .then(contact => {
+            res.json(contact)
+        })
+        .catch(e => {
+            console.log(e)
+            res.json({
+                message: 'Error Occurred'
+            })
+        })
 }
